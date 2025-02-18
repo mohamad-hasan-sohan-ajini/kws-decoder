9336d51fa50c8b8e9357870be8aad6275e24fb81
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-02-18 23:15:20 +0330

    Add Unicode test

--- decoder_test.py
+++ decoder_test.py
@@ -9,6 +9,10 @@ except ImportError:
     from wrapper import KWSDecoder
 from beam_search import prefix_beam_search
 
+print("###################")
+print("# ENGLISH example #")
+print("###################")
+
 with open('resources/labels.json') as f:
     labels = json.load(f)
 
@@ -29,4 +33,29 @@ print('c++_result:')
 decoder = KWSDecoder(labels, 0)
 decoder.add_words(keywords)
 result = decoder.search(data)
-print(result)
+print(json.dumps(result, indent=4))
+
+print("\n" * 3)
+print("###################")
+print("# PERSIAN example #")
+print("###################")
+with open('resources/fa_labels.json') as f:
+    fa_labels = json.load(f)
+
+fa_data = torch.load('resources/fa_output.pth')
+fa_data = fa_data.squeeze().numpy()
+
+fa_keywords = ["پادشاه", "پرگار", "ارادت", "جلال", "عظمت", "طالبی"]
+
+ref_result = prefix_beam_search(fa_labels, 0, fa_data, fa_keywords)
+print('ref_result:')
+for key in ref_result.keys():
+    print(f'key: {key}')
+    for keyword in ref_result[key]:
+        print(f'\t{keyword}')
+
+print('c++_result:')
+decoder = KWSDecoder(fa_labels, 0)
+decoder.add_words(fa_keywords)
+result = decoder.search(fa_data)
+print(json.dumps(result, indent=4, ensure_ascii=False))
