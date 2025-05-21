8caf67a993d83b88b216f21b79725d7a0d589782
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-05-21 20:37:09 +0330

    Organize Trie test

--- tests/trie_test.py
+++ tests/trie_test.py
@@ -1,9 +1,22 @@
-from wrapper import Trie
+import pytest
+from kws_decoder import Trie
 
-trie = Trie()
-trie.add_word('مادرید')
 
-print(trie.has_word('مادرید'))
-print(trie.has_word('مادر'))
-print(trie.is_promising('مادر'))
-print(trie.is_promising('مار'))
+def test_trie_persian():
+    trie = Trie()
+    trie.add_word("مادرید")
+
+    assert trie.has_word("مادرید") is True
+    assert trie.has_word("مادر") is False
+    assert trie.is_promising("مادر") is True
+    assert trie.is_promising("مار") is False
+
+
+def test_trie_english():
+    trie = Trie()
+    trie.add_word("hello")
+
+    assert trie.has_word("hello") is True
+    assert trie.has_word("hell") is False
+    assert trie.is_promising("hell") is True
+    assert trie.is_promising("her") is False
