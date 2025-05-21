05df1d22b1c991482301bd794b5edf03c0b53957
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-05-21 19:21:27 +0330

    Format code

--- tests/beam_search.py
+++ tests/beam_search.py
@@ -7,7 +7,17 @@ from pygtrie import CharTrie
 from tqdm import tqdm
 
 
-def prefix_beam_search(alphabet, blank_index, ctc, keywords, beam_width=16, beta=1.02, min_keyword_score=.001, max_gap=25, clip_char_prob=.01):
+def prefix_beam_search(
+    alphabet,
+    blank_index,
+    ctc,
+    keywords,
+    beam_width=16,
+    beta=1.02,
+    min_keyword_score=0.001,
+    max_gap=25,
+    clip_char_prob=0.01,
+):
     trie = CharTrie()
     for word in keywords:
         trie[word] = True
@@ -15,7 +25,7 @@ def prefix_beam_search(alphabet, blank_index, ctc, keywords, beam_width=16, beta
     result = defaultdict(list)
 
     # STEP 1: Initiliazation
-    O = ''
+    O = ""
     Pb, Pnb = defaultdict(Counter), defaultdict(Counter)
     Pb[-1][O] = 1
     Pnb[-1][O] = 0
@@ -43,7 +53,7 @@ def prefix_beam_search(alphabet, blank_index, ctc, keywords, beam_width=16, beta
                     if len(l) > 0 and c == l[-1]:
                         Pnb[t][l_plus] += ctc[t][c_ix] * Pb[t - 1][l]
                         Pnb[t][l] += ctc[t][c_ix] * Pnb[t - 1][l]
-                # END: STEP 4
+                    # END: STEP 4
 
                     # STEP 5: Extending with any other non-blank character and LM constraints
                     else:
@@ -52,13 +62,17 @@ def prefix_beam_search(alphabet, blank_index, ctc, keywords, beam_width=16, beta
 
                     # STEP 6: Make use of discarded prefixes
                     if l_plus not in A_prev:
-                        Pb[t][l_plus] += ctc[t][blank_index] * (Pb[t - 1][l_plus] + Pnb[t - 1][l_plus])
+                        Pb[t][l_plus] += ctc[t][blank_index] * (
+                            Pb[t - 1][l_plus] + Pnb[t - 1][l_plus]
+                        )
                         Pnb[t][l_plus] += ctc[t][c_ix] * Pnb[t - 1][l_plus]
                     # END: STEP 6
 
         # STEP 7: Select most probable prefixes
         A_next = Pb[t] + Pnb[t]
-        sorted_items = sorted(A_next.items(), key=lambda x: x[1] * (beta ** len(x[0])), reverse=True)
+        sorted_items = sorted(
+            A_next.items(), key=lambda x: x[1] * (beta ** len(x[0])), reverse=True
+        )
         A_prev = []
         has_empty_string = False
         for prefix, prob in sorted_items:
@@ -77,31 +91,31 @@ def prefix_beam_search(alphabet, blank_index, ctc, keywords, beam_width=16, beta
             # 1, 3
             else:
                 if len(result[prefix]):
-                    if (t * .02 - result[prefix][-1]['end'] < max_gap * .02):
-                        if result[prefix][-1]['score'] < score:
+                    if t * 0.02 - result[prefix][-1]["end"] < max_gap * 0.02:
+                        if result[prefix][-1]["score"] < score:
                             result[prefix].pop()
-                            result[prefix].append({'end': t * .02, 'score': score})
+                            result[prefix].append({"end": t * 0.02, "score": score})
                     else:
-                        result[prefix].append({'end': t * .02, 'score': score})
+                        result[prefix].append({"end": t * 0.02, "score": score})
                 else:
-                    result[prefix].append({'end': t * .02, 'score': score})
+                    result[prefix].append({"end": t * 0.02, "score": score})
                 A_prev.append(prefix)
         # END: STEP 7
         if not has_empty_string:
-            A_prev.append('')
+            A_prev.append("")
         Pb[t][O] = 1
         Pnb[t][O] = 0
         # print(f'timestep: {t}\nA_prev length: {len(A_prev)}\n{A_prev}\n')
     return result
 
 
-if __name__ == '__main__':
-    with open('resources/labels.json') as f:
+if __name__ == "__main__":
+    with open("resources/labels.json") as f:
         labels = json.load(f)
 
-    data = torch.load('resources/output.pth')
+    data = torch.load("resources/output.pth")
     data = data.squeeze().numpy()
 
-    keywords = ['think', 'sport', 'cheat', 'kids', 'remember']
+    keywords = ["think", "sport", "cheat", "kids", "remember"]
     result = prefix_beam_search(labels, 0, data, keywords)
     print(result)
